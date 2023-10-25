bbc0db3fe8e8f0d91ff0f43427b35abc31a12942
Alvino Albas <2111522016_alvino@student.unand.ac.id>
2023-10-25 22:31:57 +0700

    Menambahkan menu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,17 +14,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
-
     public void gilangkeren(View view) {
         Intent intent = new Intent(this,gambar2.class);
         startActivity(intent);
@@ -32,38 +27,42 @@ public class MainActivity extends AppCompatActivity {
     public void gilang1(View view) {
         Intent intent = new Intent(this,Gambar3Activity.class);
         startActivity(intent);
-
     }
     public void gilang3(View view) {
         Intent intent = new Intent(this,gambar4.class);
         startActivity(intent);
-
     }
     public void gilang2(View view) {
         Intent intent = new Intent(this,gambar5.class);
         startActivity(intent);
-
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater(R.menu.action_menu, menu);
-//        return true;
-//    }
-//
-//    private void getMenuInflater(int actionMenu, Menu menu) {
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+        return true;
+    }
 
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_notification:
+                // Logika untuk item menu_notification
+                return true;
+            case R.id.menu_profile:
+                // Logika untuk item menu_profile
+                return true;
+            case R.id.menu_logout:
+                // Logika untuk item menu_logout
+                return true;
+            case R.id.menu_back:
+                // Logika untuk item menu_back
+                return true;
 
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        switch (item.getItemId()){
-//            case R.id.menu_notification:
-//                Toast.makeText(getApplicationContext(), "ini adalah notif", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.menu_profile:
-//
-//
-//        }
-//        return super.onOptionsItemSelected(item);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
